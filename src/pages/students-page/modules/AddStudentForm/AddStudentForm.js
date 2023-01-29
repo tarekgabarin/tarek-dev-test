daa1ae72a6aec207130d75e64d02ce6c428518c9
tarekgabarin <tarekgabarin1992@gmail.com>
2023-01-29 16:34:57 -0500

    working on forms

--- src/pages/students-page/modules/AddStudentForm/AddStudentForm.js
+++ src/pages/students-page/modules/AddStudentForm/AddStudentForm.js
@@ -2,35 +2,73 @@
 import { useForm } from "react-hook-form";
 
 import {
-    FormErrorMessage,
-    FormLabel,
-    FormControl,
-    Input,
-    Button,
-  } from '@chakra-ui/react'
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  // Button,
+  VStack,
+  Box
+} from '@chakra-ui/react'
 
 const AddStudentForm = () => {
-    
-    const { 
-        register, 
-        handleSubmit,
-        formState: {errors, isSubmitting}
-    } = useForm();
 
-    function onSubmit(values) {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            resolve()
-          }, 3000)
-        })
-      }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm();
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}> 
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2))
+        resolve()
+      }, 3000)
+    })
+  }
 
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+
+      <VStack spacing={2}>
+
+
+          <FormControl isInvalid={errors.firstName}>
+            <FormLabel htmlFor='firstName'>First name</FormLabel>
+            <Input
+              id='firstName'
+              placeholder='name'
+              {...register('firstName', {
+                required: 'This is required'
+              })}
+            />
+            <FormErrorMessage>
+              {errors.firstName && errors.firstName.message}
+            </FormErrorMessage>
+          </FormControl>
+
+
+  
+          <FormControl isInvalid={errors.familyName}>
+            <FormLabel htmlFor='familyName'>Last name</FormLabel>
+            <Input
+              id='familyName'
+              placeholder='name'
+              {...register('familyName', {
+                required: 'This is required'
+              })}
+            />
+            <FormErrorMessage>
+              {errors.familyName && errors.familyName.message}
+            </FormErrorMessage>
+          </FormControl>
+
+
+      </VStack>
+
+    </form>
+  )
 
 }
 
