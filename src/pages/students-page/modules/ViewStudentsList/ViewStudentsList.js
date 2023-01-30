333e95dc3850fcf661fec649cf695bf7a936067c
tarekgabarin <tarekgabarin1992@gmail.com>
2023-01-29 19:59:24 -0500

    all redux stuff hooked up

--- src/pages/students-page/modules/ViewStudentsList/ViewStudentsList.js
+++ src/pages/students-page/modules/ViewStudentsList/ViewStudentsList.js
@@ -1,14 +1,44 @@
 /*eslint-disable */
 import { useSelector } from "react-redux"
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+} from '@chakra-ui/react'
 
 const ViewStudentsList = () => {
 
     const listOfStudents = useSelector(state => state.students.listOfStudents)
 
     return (
-        <div>
-            Students List
-        </div>
+        <TableContainer>
+            <Table variant='striped' colorScheme='teal'>
+                <Thead>
+                    <Tr>
+                        <Th>First Name</Th>
+                        <Th>Last Name</Th>
+                        <Th>Date of Birth</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {
+                        listOfStudents.map(student => {
+                            return (
+                                <Tr>
+                                    <Td>{student.firstName}</Td>
+                                    <Td>{student.familyName}</Td>
+                                    <Td>{student.dateOfBirth}</Td>
+                                </Tr>
+                            )
+                        })
+                    }
+                </Tbody>
+            </Table>
+        </TableContainer>
     )
 }
 
